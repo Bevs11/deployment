f15ea04484539eccc7a5c222ddd0db96dd6e024d
bevsi <beverlymateo11@gmail.com>
2023-02-23 18:58:20 +0800

    feat: Added moving background

--- p2-js-game/script.js
+++ p2-js-game/script.js
@@ -1,8 +1,14 @@
 const game = document.getElementById('game');
 const character = document.getElementById("character");
 const block = document.getElementById("block");
+const block2 = document.getElementById("block2");
 const playAgain = document.getElementById('playAgain');
 const startGame = document.getElementById('start');
+const movingGround = document.getElementById('moving-ground');
+const movingGround2 = document.getElementById('moving-ground2');
+const movingBuildings = document.getElementById('buildings');
+const movingBuildings2 = document.getElementById('buildings2');
+const timer = document.getElementById('timer');
 
 function jump() {
     if (character.classList != "animate") {
@@ -26,9 +32,18 @@ const checkHit = setInterval(function () {
     }
 }, 10);
 
+let gameOn;
 function youLose() {
     const lose = document.querySelector('#youLose');
     lose.classList.remove('youLose');
+    // block2.classList.remove('block-move2');
+    movingGround.classList.remove('moving-ground');
+    movingGround2.classList.remove('moving-ground2');
+    movingBuildings.classList.remove('moving-buildings');
+    movingBuildings2.classList.remove('moving-buildings2');
+    gameOn = 0;
+    console.log(`points: ${sec}`);
+    score.innerText = sec;
 }
 
 playAgain.addEventListener('click', function () {
@@ -37,5 +52,26 @@ playAgain.addEventListener('click', function () {
 
 startGame.addEventListener('click', function () {
     block.classList.add('block-move');
+    // block2.classList.add('block-move2');
+    movingGround.classList.add('moving-ground');
+    movingGround2.classList.add('moving-ground2');
+    movingBuildings.classList.add('moving-buildings');
+    movingBuildings2.classList.add('moving-buildings2');
     startGame.classList.add('hidden');
+    gameOn = 1;
+    setInterval(timerFunction, 1000);
 });
+
+
+let sec = 0;
+
+function timerFunction() {
+    if (gameOn == 1) {
+        console.log(sec);
+        sec++;
+    } else {
+        clearInterval(timerFunction);
+        return sec;
+    }
+}
+
