95d197d805a2aee0fbe43979dec213a36bb0aab8
bevsi <beverlymateo11@gmail.com>
2023-02-24 14:15:26 +0800

    feat: Added starry sky img and animation

--- p2-js-game/script.js
+++ p2-js-game/script.js
@@ -1,7 +1,7 @@
 const game = document.getElementById('game');
 const character = document.getElementById("character");
 const block = document.getElementById("block");
-// const block2 = document.getElementById("block2");
+const moonMove = document.getElementById("moon");
 const blockMove = document.querySelector('.block-move');
 const blockMoveRandom = document.querySelector('.moveRandom')
 const playAgain = document.getElementById('playAgain');
@@ -14,12 +14,14 @@ const timer = document.getElementById('timer');
 let currentSpeed = 0;
 
 function jump() {
-    if (character.classList != "animate") {
+
+    if (character.classList != 'animate') {
         character.classList.add("animate");
     }
 
     setTimeout(function () {
         character.classList.remove("animate");
+
     }, 500)
 }
 
@@ -33,13 +35,15 @@ const checkHit = setInterval(function () {
         block.style.display = "none";
         youLose();
     }
+
 }, 10);
 
 let gameOn;
 function youLose() {
     const lose = document.querySelector('#youLose');
     lose.classList.remove('youLose');
-    // block2.classList.remove('block-move2');
+    moonMove.classList.remove('moonAnimation');
+    moonMove.classList.add('hidden');
     movingGround.classList.remove('moving-ground');
     movingGround2.classList.remove('moving-ground2');
     movingBuildings.classList.remove('moving-buildings');
@@ -47,6 +51,8 @@ function youLose() {
     gameOn = 0;
     console.log(`points: ${sec}`);
     score.innerText = sec;
+
+
 }
 
 playAgain.addEventListener('click', function () {
@@ -54,17 +60,18 @@ playAgain.addEventListener('click', function () {
 });
 
 startGame.addEventListener('click', function () {
-    block.classList.add('block-move');
-    // block2.classList.add('block-move2');
+
+    moonMove.classList.add('moonAnimation');
     movingGround.classList.add('moving-ground');
     movingGround2.classList.add('moving-ground2');
     movingBuildings.classList.add('moving-buildings');
     movingBuildings2.classList.add('moving-buildings2');
     startGame.classList.add('hidden');
+    block.classList.add('block-move');
     gameOn = 1;
-    blockSpeed();
+
     setInterval(timerFunction, 1000);
-    setInterval(blockSpeed, currentSpeed);
+
 });
 
 
@@ -74,33 +81,19 @@ function timerFunction() {
     if (gameOn == 1) {
         console.log(sec);
         sec++;
-        blockSpeed();
+
     } else {
         clearInterval(timerFunction);
+
         return sec;
     }
 }
+
 
 
 const speed1 = [600, 700, 800, 900, 1000, 1100, 1200, 1300];
 const speed2 = [500, 600, 650, 700, 750, 800, 900, 950, 1000];
 const speed3 = [400, 450, 500, 600, 700, 800, 900, 950, 1000];
 const speed4 = [300, 400, 450, 500, 600, 700, 800, 900, 950];
-
-
-
-function blockSpeed() {
-
-    // let j = 1
-    // if (sec >= 30) {
-
-    // }
-
-    let num = Math.floor(Math.random() * speed1.length);
-    currentSpeed = speed1[num];
-    blockMoveRandom.style.animationDuration = `${speed1[num]}ms`;
-    console.log(parseInt(speed1[num]));
-}
-
 
 
