752a4eff12b0f29f017a2273f1149de529e43557
bevsi <beverlymateo11@gmail.com>
2023-03-13 17:14:29 +0800

    feat: Removed lines with no code and revised Css codes

--- p2-js-game/script.js
+++ p2-js-game/script.js
@@ -2,11 +2,11 @@ const game = document.getElementById('game');
 const difficulty = document.getElementById('difficulty');
 const easyBtn = document.getElementById('easy');
 const hardBtn = document.getElementById('hard');
-const character = document.getElementById("character");
-const block1 = document.getElementById("block");
-const block2 = document.getElementById("block2");
-const star = document.getElementById("star");
-const moonMove = document.getElementById("moon");
+const character = document.getElementById('character');
+const block1 = document.getElementById('block');
+const block2 = document.getElementById('block2');
+const star = document.getElementById('star');
+const moonMove = document.getElementById('moon');
 const playAgain = document.getElementById('playAgain');
 const startGame = document.getElementById('start');
 const movingGround = document.getElementById('moving-ground');
@@ -31,12 +31,11 @@ const obstruction = [{
     speed: 12
 }]
 
-
-let hard = 0;
+//Sets the difficulty of the game
+let hard = 0; //difficulty of the game 0-easy and 1-hard
 easyBtn.addEventListener('click', function () {
     startGame.classList.remove('hidden');
     difficulty.classList.add('hidden');
-
 });
 hardBtn.addEventListener('click', function () {
     startGame.classList.remove('hidden');
@@ -44,96 +43,92 @@ hardBtn.addEventListener('click', function () {
     hard = 1;
 });
 
-
-
+// Pulls the character to the ground after it jumps
 let gravity = setInterval(function () {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
     if (characterTop < 130) {
-        character.style.top = (characterTop + 3) + "px";
+        character.style.top = (characterTop + 3) + 'px';
     }
 }, 25);
 
+// Lets the character jump
 function jump() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if (characterTop >= 70) { //return to 130
-        character.style.top = (characterTop - 80) + "px";
-
+        character.style.top = (characterTop - 80) + 'px';
     }
 }
-addEventListener("keyup", jump);
 
-let playerStarPoints = 0;
+addEventListener('keyup', jump); // Allows the character to jump by pressing any key
+
+// Creates movement for the Star
 function starMove() {
     if (gameOn == 1) {
-
         let starLeft = parseInt(window.getComputedStyle(star).getPropertyValue('left'));
         if (starLeft <= -30) {
-            star.style.left = "500px";
+            star.style.left = '500px';
             star.style.visibility = 'visible';
         } else {
             star.style.left = (starLeft - 3) + "px";
         }
     }
-
 }
 
+// Generates random number to choose from 'obstruction' array
 let num = 0;
-function blockMove1(seconds) {
-    if (gameOn == 1) {
-        const interval = setInterval(() => {
-            let blockLeft1 = parseInt(window.getComputedStyle(block1).getPropertyValue('left'));
-            if (blockLeft1 <= -30) {
-                block1.style.left = "500px";
-                clearInterval(interval);
-                gameYes();
-                block1.src = obstruction[num].source;
-            } else {
-                block1.style.left = (blockLeft1 - 3) + "px";
-            }
-        }, seconds)
-    }
-}
 function gameYes() {
     num = Math.floor(Math.random() * 4);
     blockMove1(obstruction[num].speed);
 }
 
-
-function blockMove2() {
+//Creates movement for the 1st block
+function blockMove1(seconds) {
     if (gameOn == 1) {
-
-        let blockLeft2 = parseInt(window.getComputedStyle(block2).getPropertyValue('left'));
-        if (blockLeft2 <= -30) {
-
-            block2.style.left = "500px";
-        } else {
-            block2.style.left = (blockLeft2 - 3) + "px";
-        }
+        const interval = setInterval(() => {
+            let blockLeft1 = parseInt(window.getComputedStyle(block1).getPropertyValue('left'));
+            if (blockLeft1 <= -30) {
+                block1.style.left = '500px';
+                clearInterval(interval);
+                gameYes();
+                block1.src = obstruction[num].source;
+            } else {
+                block1.style.left = (blockLeft1 - 3) + 'px';
+            }
+        }, seconds)
     }
-
-
 }
 
-
-
-
-const checkHit = setInterval(function () {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top")); // position of character
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left")); //position of block
-    let blockLeft2 = parseInt(window.getComputedStyle(block2).getPropertyValue("left")); //position of block
-    if ((blockLeft < 50 || blockLeft2 < 50) && (blockLeft > 30 || blockLeft2 > 30) && characterTop >= 130) {
-        block.style.animation = "none";
-        block.style.display = "none";
-        block2.style.animation = "none";
-        block2.style.display = "none";
-        youLose();
+// Creates movement for the 2nd block. This is only for the hard mode
+function blockMove2() {
+    if (gameOn == 1) {
+        let blockLeft2 = parseInt(window.getComputedStyle(block2).getPropertyValue('left'));
+        if (blockLeft2 <= -30) {
+            block2.style.left = '500px';
+        } else {
+            block2.style.left = (blockLeft2 - 3) + 'px';
+        }
     }
+}
 
+//Checks if the character hits the obstruction
+const checkHit = setInterval(function () {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top')); // position of character
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left')); //position of block
+    let blockLeft2 = parseInt(window.getComputedStyle(block2).getPropertyValue('left')); //position of block
+    if ((blockLeft < 50 || blockLeft2 < 50) && (blockLeft > 30 || blockLeft2 > 30) && characterTop >= 130) {
+        block.style.animation = 'none';
+        block.style.display = 'none';
+        block2.style.animation = 'none';
+        block2.style.display = 'none';
+        youLose(); // function to end the game
+    }
 }, 15);
 
+// Points counter
+let playerStarPoints = 0;
 const starPoints = setInterval(function () {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top")); // position of character
-    let starLeft = parseInt(window.getComputedStyle(star).getPropertyValue("left")); //position of star
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top')); // position of character
+    let starLeft = parseInt(window.getComputedStyle(star).getPropertyValue('left')); //position of star
     if (gameOn == 1) {
         playerCurrentScore.innerHTML = `Score: ${playerStarPoints}`;
     }
@@ -145,11 +140,10 @@ const starPoints = setInterval(function () {
         }
         star.style.visibility = 'hidden';
     }
-
 }, 100);
 
+// function to end the game
 let gameOn;
-
 function youLose() {
     bgm.pause();
     const lose = document.querySelector('#youLose');
@@ -164,14 +158,14 @@ function youLose() {
     gameOn = 0;
     score.innerText = `${playerStarPoints}`;
     playerCurrentScore.classList.add('hidden');
-
 }
 
+// To restart the game
 playAgain.addEventListener('click', function () {
-    location.reload(); //refresh
+    location.reload(); //refresh page
 });
 
-
+// To start the game
 startGame.addEventListener('click', function () {
     bgm.play();
     gameOn = 1;
@@ -186,9 +180,7 @@ startGame.addEventListener('click', function () {
     }
     setInterval(starMove, 18);
     blockMove1(12);
-
 });
-
 
 
 
